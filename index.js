df0c9b06653b4c7de8614151f12467a2b920fc70
EconClass <zurich.okoren@gmail.com>
2020-01-27 09:43:29 -0800

    [add] data manipulation stuff

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 app.use(express.static('public'));
 
-const PORT = process.env.PORT = 4000;
+const PORT = process.env.PORT = 8080;
 
 app.listen(PORT, () => {
   console.log('Server is running at:', PORT);
--- public/titanic.js
+++ public/titanic.js
@@ -1,22 +1,63 @@
 fetch('titanic-passengers.json')
   .then(res => res.json())
-  .then(json => console.log(json))
+  .then(json => {
+    console.log(handleData(json))
+  })
   .catch(err => console.log(err.message))
 
 
-
-function countFields(data, field, value) {
+const countFields = (data, field, value) => {
   return data.reduce((acc, passenger) => {
     if (passenger.fields[field] === value) {
-      return acc += 1
+      return acc += 1;
     }
-    return acc
+    return acc;
   }, 0)
 }
 
 
+const passengerInfo = (passenger) => {
+  const info = passenger.fields;
+  const { name, fare, pclass, survived, age } = info;
+  return {
+    name,
+    fare,
+    pclass,
+    survived,
+    age
+  }
+};
 
-function handleData(data) {
+const minOfField = (data, field) => {
+  // Initial min is the first passenger's value
+  let minField = data[0].fields[field]
+
+  data.forEach(passenger => {
+    if (passenger.fields[field] < minField && passenger.fields[field] !== undefined) {
+      minField = passenger.fields[field];
+    }
+  });
+
+  return minField
+}
+
+const maxOfField = (data, field) => {
+  // Initial min is the first passenger's value
+  let maxField = data[0].fields[field]
+
+  data.forEach(passenger => {
+    if (passenger.fields[field] > maxField && passenger.fields[field] !== undefined) {
+      maxField = passenger.fields[field];
+    }
+  });
+
+  return maxField
+}
+
+
+const handleData = (data) => {
+  // Total Passengers
+  console.log(`Total Passengers: ${data.length}`)
   // Number of women
   const womenCount = countFields(data, 'sex', 'female')
   console.log(`Women count: ${womenCount}`)
@@ -24,4 +65,26 @@ function handleData(data) {
   // Number of men
   const menCount = countFields(data, 'sex', 'male')
   console.log(`Men count: ${menCount}`)
+
+  // First Passenger Info
+  const { name, fare, pclass, survived, age } = passengerInfo(data[0])
+  console.log(`
+  First Passenger: ${name}
+  Fare: ${fare}
+  Class: ${pclass}
+  Survived: ${survived}
+  Age: ${age}
+  `)
+
+  // Number of survivors
+  const survivors = countFields(data, 'survived', 'Yes')
+  console.log(`Survived: ${survivors}`)
+
+  // Number of dead
+  const dead = countFields(data, 'survived', 'No')
+  console.log(`Died: ${dead}`)
+
+  console.log(`Min Fare: ${minOfField(data, "fare")}`)
+
+  console.log(`Max Fare: ${maxOfField(data, "fare")}`)
 }
